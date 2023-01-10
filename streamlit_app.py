b2161a05b6a775f796ecd59f64f7c164bae18e8a
Srinivasa Rao Yalavarthi <121909660+srinisnowflake@users.noreply.github.com>
2023-01-09 20:55:25 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas
 import requests
-import snowflake.connector
+
 from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
@@ -43,7 +43,7 @@ try:
   
   
 
-
+import snowflake.connector
 
 my_cnx = snowflake.connector.connect(streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
